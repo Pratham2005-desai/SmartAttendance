bfd1e91ecec77dcfd5dfe2ec31a296f3f8e346da
Pratham2005-desai <pratham.k.desai@tsuniv.edu.in>
2025-06-26 16:51:29 +0530

    Add files via upload

---
+++ server/db.py
@@ -0,0 +1,9 @@
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+mongo_uri = os.getenv("MONGO_URI")
+client = MongoClient(mongo_uri)
+db = client["attendance_db"]
